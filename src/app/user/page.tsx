d03a76b30fba6160010dbafd7e5858040a1113fa
Aimable01 <aimable.kwizera14@gmail.com>
2024-05-07 15:48:09 +0200

    user login protect

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,17 +1,34 @@
 "use client";
-
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function UserPage() {
-  const handleSignOut = () => {
-    signOut({ redirect: false, callbackUrl: "http://localhost:3000" });
+  const router = useRouter();
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/login");
+    },
+  });
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push("/");
   };
+
+  console.log(session);
+
+  if (status == "loading") {
+    return null;
+  }
 
   return (
     <div>
-      {" "}
-      <h1 className="text-slate-50">This is the logged in user</h1>{" "}
-      <button className="text-slate-50" onClick={() => signOut()}>
+      <h1 className="text-slate-50">
+        This is the logged in user{" "}
+        {status === "authenticated" ? session.user?.name : ""}{" "}
+      </h1>
+      <button className="text-slate-50" onClick={handleSignOut}>
         Sign out
       </button>
     </div>
