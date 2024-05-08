51531169e216ddf52c150d9c986b65c67d3e1432
Aimable01 <aimable.kwizera14@gmail.com>
2024-05-08 08:25:03 +0200

    buttons

--- src/app/ui/buttons/guest.tsx
+++ src/app/ui/buttons/guest.tsx
@@ -1,15 +1,43 @@
+"use client";
+import { useState } from "react";
 import { VscAccount } from "react-icons/vsc";
+import { FaSpinner } from "react-icons/fa";
 
 export default function GuestBtn() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setIsDisabled(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsDisabled(false);
+    }, 5000);
+  };
+
   return (
-    <div className="bg-blue-800 rounded-md px-4 py-2 text-white hover:bg-blue-900 focus:bg-blue-950">
+    <div
+      className={`bg-blue-800 rounded-md px-4 py-2 text-white hover:bg-blue-900 focus:bg-blue-950 ${
+        isDisabled ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+      onClick={handleClick}
+    >
       <div className="flex items-center gap-1">
         <h2 className="text-xl font-semibold">
-          {" "}
-          <VscAccount />
+          {isLoading ? <Loader /> : <VscAccount />}
         </h2>
         Guest
       </div>
     </div>
   );
 }
+
+export const Loader = () => {
+  return (
+    <div className="loader">
+      <FaSpinner className="text-xl animate-spin" />
+    </div>
+  );
+};
--- src/app/ui/buttons/login.tsx
+++ src/app/ui/buttons/login.tsx
@@ -1,13 +1,35 @@
+"use client";
 import { IoIosLogIn } from "react-icons/io";
+import { FaSpinner } from "react-icons/fa";
+import { useState } from "react";
+import { Loader } from "./guest";
 
 export default function LoginBtn() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsDisabled(true);
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsDisabled(false);
+      setIsLoading(false);
+    }, 5000);
+  };
+
   return (
-    <div className="bg-blue-800 rounded-md px-4 py-2 text-white hover:bg-blue-900 focus:bg-blue-950">
+    <div
+      onClick={handleClick}
+      className={`bg-blue-800 rounded-md px-4 py-2 text-white hover:bg-blue-900 focus:bg-blue-950 ${
+        isDisabled ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+    >
       <div className="flex items-center gap-1">
         Login{" "}
         <h2 className="text-xl font-semibold">
           {" "}
-          <IoIosLogIn />
+          {isLoading ? <Loader /> : <IoIosLogIn />}
         </h2>
       </div>
     </div>
