940f510d777046e9e6c22353f743303bcfdd02d5
Aimable01 <aimable.kwizera14@gmail.com>
2024-05-08 11:42:07 +0200

    register form

--- src/app/register/form.tsx
+++ src/app/register/form.tsx
@@ -1,26 +1,53 @@
 "use client";
-import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useState } from "react";
 import { IoMdEye } from "react-icons/io";
 import { IoIosEyeOff } from "react-icons/io";
+import { useRouter } from "next/navigation";
+import { Loader } from "../ui/buttons/guest";
+
+// the types of inputs
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 export default function Form() {
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
 
   const [viewPassword, setViewPassword] = useState<boolean>(false);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+  const schema = z.object({
+    name: z.string().nonempty({ message: "Please enter your name" }),
+    email: z
+      .string()
+      .nonempty({ message: "Please enter your email" })
+      .email({ message: "Please enter a valid email" }),
+    password: z
+      .string()
+      .min(5, { message: "Please enter atleast 5 characters" })
+      .nonempty({ message: "Please enter your password" }),
+  });
 
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (data: object) => {
     const response = await fetch(`/api/auth/register`, {
       method: "POST",
-      body: JSON.stringify({
-        name,
-        email,
-        password,
-      }),
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        router.push("/login");
+      }
     });
 
     console.log({ response });
@@ -32,7 +59,7 @@ export default function Form() {
         <div className="m-5 text-center text-white">
           <h2>Please register to continue</h2>
         </div>
-        <form onSubmit={handleSubmit} action="" className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
           <div>
             <label className="text-white text-[14px]" htmlFor="name">
               Name
@@ -41,11 +68,12 @@ export default function Form() {
               <input
                 className="px-2 py-1 rounded text-sm focus:outline-none"
                 type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
+                {...register("name")}
                 placeholder="Enter full name"
               />
+              {errors.name && (
+                <p className="text-pink-500 text-sm">{errors.name.message}</p>
+              )}
             </div>
           </div>
           <div>
@@ -55,12 +83,12 @@ export default function Form() {
             <div>
               <input
                 className="px-2 py-1 rounded text-sm focus:outline-none"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
+                {...register("email")}
                 placeholder="Enter email"
               />
+              {errors.email && (
+                <p className="text-pink-500 text-sm">{errors.email.message}</p>
+              )}
             </div>
           </div>
           <div>
@@ -71,9 +99,7 @@ export default function Form() {
               <input
                 className="px-2 py-1 rounded text-sm focus:outline-none"
                 type={viewPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
+                {...register("password")}
                 placeholder="Enter password"
               />
               <button
@@ -85,11 +111,19 @@ export default function Form() {
               >
                 {viewPassword ? <IoIosEyeOff /> : <IoMdEye />}
               </button>
+              {errors.password && (
+                <p className="text-pink-500 text-sm">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
           </div>
           <div>
-            <button className="bg-blue-600 text-white px-3 py-1 rounded-md text-[14px]">
-              Register
+            <button
+              disabled={isSubmitting}
+              className="flex items-center gap-1 bg-blue-600 text-white px-3 py-1 rounded-md text-[14px]"
+            >
+              {isSubmitting ? <Loader /> : ""} <p>Register</p>
             </button>
           </div>
         </form>
